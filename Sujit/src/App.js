d8c212d994bba71e07103ddf9f46490e869260b6
sujit <ssaroj9519@gmail.com>
2022-03-12 13:54:31 +0530

    First commit

---
+++ Sujit/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import Todo from './component/todoreact/todo';
+import Temp from './component/weather/temp';
+// import Resturant from "./component/Basics/Resturant";
+// import UseState from './component/Hooks/useState';
+// import UseEffect from './component/Hooks/useEffect';
+// import UseReducer from './component/Hooks/useReducer';
+
+const App = () => {
+  return <Temp />
+}
+
+export default App
---
+++ Sujit/src/component/weather/temp.js
@@ -0,0 +1,71 @@
+// api.openweathermap.org/data/2.5/weather?q=mumbai&appid=204665662adc048b963132a25c2754e0
+import React, {useState, useEffect} from 'react';
+import Weathercard from './weathercard';
+import "./style.css";
+
+const Temp = () => {
+
+    const [searchValue,setSearchValue] = useState("Mumbai");
+    const [tempInfo, setTempInfo] = useState({});
+
+    const getWeatherInfo = async () => {
+        try{
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=204665662adc048b963132a25c2754e0`;
+
+            let res = await fetch(url);
+            let data = await res.json();
+
+            const { temp, humidity, pressure } = data.main;
+            const {main:weathermood} = data.weather[0];
+            const{name}= data;
+            const{speed}= data.wind;
+            const{country, sunset} = data.sys;
+
+            const myNewWeatherInfo = {
+                temp,
+                humidity,
+                pressure,
+                weathermood,
+                name,
+                speed,
+                country,
+                sunset,
+                
+
+
+            };
+
+            setTempInfo(myNewWeatherInfo);
+
+
+
+            // console.log(temp);
+        } catch(error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getWeatherInfo();
+    }, []);
+
+
+    return (
+        <>
+        <div className="wrap">
+            <div className="search">
+                <input type="search" placeholder='search..' autoFocus id='search' className='searchTerm' value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+                    <button className="searchButton" type="button" onClick={getWeatherInfo}>
+                        Search
+                    </button>
+            </div>
+        </div>
+        {/* our temp card */}
+        <Weathercard tempInfo={tempInfo} />
+       
+            
+        </>
+    )
+}
+
+export default Temp;
